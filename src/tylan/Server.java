5e92f387a7ee0da10b6c3b8defda8cfa71754645
@tyvanrjhb024 <tyvanrjhb024@student.wethinkcode.co.za>
2025-05-31 17:58:10 +0200

    edited server class

--- src/tylan/Server.java
+++ src/tylan/Server.java
@@ -1,16 +1,22 @@
 package tylan;
 
 
-import java.io.BufferedReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server{
     static BufferedReader in;
     static PrintWriter out;
-    static int Port;
+    static int port = 8080;
+    static ServerSocket listeningSocket;
+    static Socket communicationSocket;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("The server is running");
-
+        listeningSocket = new ServerSocket(port);
+        communicationSocket = listeningSocket.accept();
+        out = new PrintWriter(communicationSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(communicationSocket.getInputStream()));
     }
 }
