5620cdf3da081868c38eb17e181aa744a72dc9f5
@tyvanrjhb024 <tyvanrjhb024@student.wethinkcode.co.za>
2025-05-31 17:39:11 +0200

    new files added and edited server class

--- src/tylan/Server.java
+++ src/tylan/Server.java
@@ -1,9 +1,16 @@
 package tylan;
 
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
 public class Server{
+    static BufferedReader in;
+    static PrintWriter out;
+    static int Port;
 
     public static void main(String[] args){
         System.out.println("The server is running");
+
     }
 }
