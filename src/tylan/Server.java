4338f3721b82b3c5982e3a99dfa8c48cb2dfeeb9
@tyvanrjhb024 <tyvanrjhb024@student.wethinkcode.co.za>
2025-06-01 11:47:20 +0200

    adding error handling

--- src/tylan/Server.java
+++ src/tylan/Server.java
@@ -6,7 +6,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server{
+public class Server {
     static BufferedReader in;
     static PrintWriter out;
     static int port = 8080;
@@ -20,17 +20,21 @@ public class Server{
         System.out.println("Client connected");
 
 
-
         out = new PrintWriter(communicationSocket.getOutputStream(), true);
         out.println("Welcome to the Mini Calculator App! Let's do some quick math.");
-
-
-        while (true) {
-            out.println();
-        }
         in = new BufferedReader(new InputStreamReader(communicationSocket.getInputStream()));
-        String clientResponse = in.readLine();
-        System.out.println(clientResponse);
+
+
+        String clientResponse;
+        do {
+            out.println("choose a valid operator, options : [+, -, *, /]");
+            clientResponse = in.readLine();
+        }while (verifyOperator(clientResponse).equals("Invalid Operator"));
+
+
+
+
+
         communicationSocket.close();
         listeningSocket.close();
 
@@ -41,7 +45,38 @@ public class Server{
          */
     }
 
-    public int verifyNumber(String number){
+    public static int verifyNumber(String numString, Socket socket) throws IOException {
+        int number;
+        while (true) {
 
+            PrintWriter response = new PrintWriter(socket.getOutputStream(), true);
+            try {
+                number = Integer.parseInt(numString);
+                break;
+            } catch (NumberFormatException e) {
+                response.println("Please enter a valid number!");
+            }
+        }
+
+        return number;
+    }
+
+    public static String verifyOperator(String operator) {
+        String[] operators = {"+", "-", "/", "*"};
+        boolean found = false;
+
+
+        for (String theOperator : operators) {
+            if (operator == theOperator) {
+                found = true;
+                break;
+            }
+        }
+        if (found){
+            return operator;
+        }
+        else {
+            return "Invalid operator";
+        }
     }
 }
